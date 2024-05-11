8924f35e1bf1ab6d1c1204dce22c6a78005211fc
AndrewKyslyi <skorpypro@gmail.com>
2024-05-11 15:01:11 +0300

    cmake new files

---
+++ build/CMakeFiles/OpenGLPj.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLPj.dir/src/main.cpp.o"
+  "CMakeFiles/OpenGLPj.dir/src/main.cpp.o.d"
+  "OpenGLPj"
+  "OpenGLPj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLPj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
